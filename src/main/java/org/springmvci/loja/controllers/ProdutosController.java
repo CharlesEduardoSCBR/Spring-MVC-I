f3437dd4dd50ba8bcf9086d936b3c89c8b0d02a2
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-03-30 19:49:54 -0300

    C1M02.06.01 - Criando o nosso controller de produtos

---
+++ src/main/java/org/springmvci/loja/controllers/ProdutosController.java
@@ -0,0 +1,13 @@
+package org.springmvci.loja.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProdutosController {
+
+	@RequestMapping("/produtos/form")
+	public String form(){
+		return "produtos/form";
+	}
+}
