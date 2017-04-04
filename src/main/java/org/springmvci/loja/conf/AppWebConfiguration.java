14bb656060f51c22c3e5c37dc42f45c3b43963c8
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-04-04 12:09:04 -0300

    C1M02.15.01 - Fazendo o spring encontrar os daos

--- src/main/java/org/springmvci/loja/conf/AppWebConfiguration.java
+++ src/main/java/org/springmvci/loja/conf/AppWebConfiguration.java
@@ -5,9 +5,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springmvci.loja.controllers.HomeController;
+import org.springmvci.loja.daos.ProdutoDAO;
 
 @EnableWebMvc
-@ComponentScan(basePackageClasses = { HomeController.class })
+@ComponentScan(basePackageClasses = { HomeController.class, ProdutoDAO.class })
 public class AppWebConfiguration {
 
 	@Bean
