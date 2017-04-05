435183386c09a92df33a98ee8c8556cafbe5933e
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-04-05 19:53:18 -0300

    C1M04.06.01 - Passando a lista de produtos para a view

--- src/main/java/org/springmvci/loja/controllers/ProdutosController.java
+++ src/main/java/org/springmvci/loja/controllers/ProdutosController.java
@@ -3,6 +3,7 @@ package org.springmvci.loja.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springmvci.loja.daos.ProdutoDAO;
 import org.springmvci.loja.models.Produto;
@@ -16,16 +17,24 @@ public class ProdutosController {
 	
 	@RequestMapping("/produtos/form")
 	public ModelAndView form(){
-		ModelAndView modelAndView = new ModelAndView();
+		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
 		
 		return modelAndView;
 	}
 	
-	@RequestMapping("/produtos")
+	@RequestMapping(value="/produtos", method = RequestMethod.GET)
 	public String gravar(Produto produto){
 		produtoDAO.gravar(produto);
 		
 		return "produtos/ok";
+	}
+	
+	@RequestMapping(value="/produtos", method = RequestMethod.GET)
+	public ModelAndView listar(){
+		ModelAndView modelAndView = new ModelAndView("produtos/lista");
+		modelAndView.addObject("produtos", produtoDAO.listar());
+		
+		return modelAndView;
 	}
 }
