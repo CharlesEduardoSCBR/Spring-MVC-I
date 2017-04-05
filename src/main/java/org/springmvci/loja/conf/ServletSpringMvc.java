9247dd6e35a3a0f738732780eb853b1ef59294fd
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-04-05 20:35:22 -0300

    C1M04.09.01 - Resolvendo problemas de encondig

--- src/main/java/org/springmvci/loja/conf/ServletSpringMvc.java
+++ src/main/java/org/springmvci/loja/conf/ServletSpringMvc.java
@@ -1,5 +1,8 @@
 package org.springmvci.loja.conf;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -11,12 +14,20 @@ public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletI
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return new Class[]{AppWebConfiguration.class, JPAConfiguration.class};
+		return new Class[] { AppWebConfiguration.class, JPAConfiguration.class };
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		return new String[]{"/"};
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+		encodingFilter.setEncoding("UTF-8");
+
+		return new Filter[] { encodingFilter };
 	}
 
 }
--- src/main/java/org/springmvci/loja/controllers/ProdutosController.java
+++ src/main/java/org/springmvci/loja/controllers/ProdutosController.java
@@ -1,5 +1,7 @@
 package org.springmvci.loja.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,8 +34,9 @@ public class ProdutosController {
 	
 	@RequestMapping(value="/produtos", method = RequestMethod.GET)
 	public ModelAndView listar(){
+		List<Produto> produtos = produtoDAO.listar();
 		ModelAndView modelAndView = new ModelAndView("produtos/lista");
-		modelAndView.addObject("produtos", produtoDAO.listar());
+		modelAndView.addObject("produtos", produtos);
 		
 		return modelAndView;
 	}
