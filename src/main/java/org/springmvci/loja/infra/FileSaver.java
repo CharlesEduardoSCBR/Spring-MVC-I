9723b8fa67c0b2df4518468db0a9829dc3d464f9
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-04-11 23:04:15 -0300

    C1M09.06 - Isolando o código de upload de arquivos

---
+++ src/main/java/org/springmvci/loja/infra/FileSaver.java
@@ -0,0 +1,30 @@
+package org.springmvci.loja.infra;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileSaver {
+
+	@Autowired
+	private HttpServletRequest request;
+	
+	public String grava(String baseFolder, MultipartFile file){
+		String realPath = request.getServletContext().getRealPath("/" + baseFolder);
+		String path = realPath + "/" + file.getOriginalFilename();
+		
+		try {
+			file.transferTo(new File(path));
+		} catch (IllegalStateException | IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return path;
+	}
+}
