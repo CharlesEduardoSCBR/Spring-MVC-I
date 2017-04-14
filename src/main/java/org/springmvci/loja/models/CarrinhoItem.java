fb684fba15df14dd51def79ff00482df78c2f14f
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-04-14 18:57:25 -0300

    C1M11.05 - Criando a classe que representa o carrinho

--- src/main/java/org/springmvci/loja/models/CarrinhoItem.java
+++ src/main/java/org/springmvci/loja/models/CarrinhoItem.java
@@ -22,6 +22,32 @@ public class CarrinhoItem {
 	public void setTipoPreço(TipoPreco tipoPreço) {
 		this.tipoPreço = tipoPreço;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
+		result = prime * result + ((tipoPreço == null) ? 0 : tipoPreço.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CarrinhoItem other = (CarrinhoItem) obj;
+		if (produto == null) {
+			if (other.produto != null)
+				return false;
+		} else if (!produto.equals(other.produto))
+			return false;
+		if (tipoPreço != other.tipoPreço)
+			return false;
+		return true;
+	}
 }
