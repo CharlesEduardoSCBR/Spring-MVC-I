f6ecdde32faf75dc712267d639ff9cc8631a41ed
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-04-15 10:38:59 -0300

    C1M12.all - session

--- src/main/java/org/springmvci/loja/controllers/CarrinhoComprasController.java
+++ src/main/java/org/springmvci/loja/controllers/CarrinhoComprasController.java
@@ -1,8 +1,10 @@
 package org.springmvci.loja.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 import org.springmvci.loja.daos.ProdutoDAO;
 import org.springmvci.loja.models.CarrinhoItem;
@@ -11,6 +13,7 @@ import org.springmvci.loja.models.TipoPreco;
 
 @Controller
 @RequestMapping("/carrinho")
+@Scope(value=WebApplicationContext.SCOPE_REQUEST)
 public class CarrinhoComprasController {
 
 	@Autowired
