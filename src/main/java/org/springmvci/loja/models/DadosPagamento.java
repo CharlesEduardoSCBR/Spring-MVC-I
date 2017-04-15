2a710dd19f77189aa7c1ef7691e864acfc2dbe71
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-04-15 19:28:18 -0300

    C1M14.09 --- dados pagamento

---
+++ src/main/java/org/springmvci/loja/models/DadosPagamento.java
@@ -0,0 +1,20 @@
+package org.springmvci.loja.models;
+
+import java.math.BigDecimal;
+
+public class DadosPagamento {
+
+	private BigDecimal value;
+	
+	public DadosPagamento(BigDecimal value){
+		this.value = value;
+	}
+	
+	public DadosPagamento() {
+	}
+	
+	public BigDecimal getValue(){
+		return value;
+	}
+	
+}
