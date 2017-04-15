55e23c2c6836f07c9f0422550afdb3e8389d1239
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-04-15 19:26:01 -0300

    C1M14.09 - Consumindo um serviço

--- src/main/java/org/springmvci/loja/controllers/PagamentoController.java
+++ src/main/java/org/springmvci/loja/controllers/PagamentoController.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -18,8 +19,13 @@ public class PagamentoController {
 	@Autowired
 	CarrinhoCompras carrinho;
 	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	@RequestMapping(value="/finalizar", method=RequestMethod.POST)
 	public ModelAndView finalizar(RedirectAttributes model){
+		String uri = "http://book-payment.herokuapp.com/payment";
+		String response = restTemplate.postForObject(uri, new DadosPagamento(carrinho.getTotal()), String.class);
 		System.out.println(carrinho.getTotal());
 		model.addFlashAttribute("sucesso", "Pagamento Realizado com Sucesso");
 		return new ModelAndView("redirect:/produtos");
