5e6a7687272e6904a29962732717da94d61007f8
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-04-21 11:04:27 -0300

    C2M2.05 a fim - json

--- src/main/java/org/springmvci/loja/conf/AppWebConfiguration.java
+++ src/main/java/org/springmvci/loja/conf/AppWebConfiguration.java
@@ -1,5 +1,7 @@
 package org.springmvci.loja.conf;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
@@ -13,10 +15,13 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springmvci.loja.controllers.HomeController;
 import org.springmvci.loja.daos.ProdutoDAO;
@@ -80,5 +85,18 @@ public class AppWebConfiguration {
 		manager.setCacheBuilder(builder);
 		
 		return manager;
+	}
+	
+	@Bean
+	public ViewResolver contentNegotiationViewResolver(ContentNegotiationManager manager){
+		List<ViewResolver> viewResolvers = new ArrayList<>();
+		viewResolvers.add(internalResourceViewResolve());
+		viewResolvers.add(new JsonViewResolver());
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(viewResolvers);
+		resolver.setContentNegotiationManager(manager);
+		
+		return resolver;
 	}
 }
